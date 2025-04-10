3266037a23fac0bd8d77c378593954f0915134b1
Prasanta Ku.Nayak <prasantan154@gmail.com>
2025-04-10 18:36:25 +0530

    last commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,103 @@
-import React from 'react'
-import "./App.css"
+import { useRef, useState } from 'react'
+import './App.css'
+import screenshot from "html2canvas"
 
-export default function App() {
+export default function App(){
 
-  const textColor=(colorName)=>{
-    document.getElementById('text').style.color=colorName
+  const[tcolor, setTColor] = useState('000')
+  const[bgCol, setBgCol] = useState('000')
+  const image = useRef()
+
+  const textColor= (colorName)=>{
+    image.current.style.color=colorName
   }
-  const backColor=(colorName)=>{
-    document.getElementById('text').style.backgroundColor=colorName
+
+  const bgColor= (colorName)=>{
+    image.current.style.backgroundColor=colorName
   }
-  return (
-   <>
-   <h1 id="heading">Font Fusion</h1>
-   <div className='container'>
-      <div id= 'container1'>
-        <h1 contentEditable='true' id='text'>Write your Text...</h1>
-        <button id='down'>Download</button>
-      </div>
-      <div id='container2'>
-        <div id='font-color'>
-          <h2 id='text-h'>Choose your text Color</h2>
-          <div id='font-color-names'>
-          <button id='red' onClick={()=>{textColor('red')}}></button>
-          <button id='blue' onClick={()=>{textColor('blue')}}></button>
-          <button id='yellow' onClick={()=>{textColor('yellow')}}></button>
-          <button id='orange' onClick={()=>{textColor('orange')}}></button>
-          <button id='pink' onClick={()=>{textColor('pink')}}></button>
-          <input type="color" />
-          </div>
-        </div>
-        <div id='bg-color'>
-         <h2 id='text-h'> Choose your background color</h2>
-         <div id='bg-color-names'>
-         <button id='red' onClick={()=>{backColor('red')}}></button>
-          <button id='blue' onClick={()=>{backColor('blue')}}></button>
-          <button id='yellow' onClick={()=>{backColor('yellow')}}></button>
-          <button id='orange' onClick={()=>{backColor('orange')}}></button>
-          <button id='pink' onClick={()=>{backColor('pink')}}></button>
-         
-          <input type="color" />
-          </div>
-        </div>
-        <div id='font-family'></div>
-      </div>
 
-   </div>
+  const fontFamily = (fontName) =>{
+    image.current.style.fontFamily=fontName
+  }
 
-   </>
-  )
+  const downloadImage = () =>{
+    screenshot(image.current).then(canvas => {
+      const imageData = canvas.toDataURL("image/png")
+      const link = document.createElement('a')
+      link.href=imageData
+      link.download="font-fusion.png"
+      link.click()
+  });
+  }
+
+
+  return(
+    <>
+
+
+        <div className='heading'>
+          <h1 id="heading">Font Fusion</h1>
+        </div>
+        
+        <div className='parent'>
+            <div id='child1'>
+                <h1 ref={image} contentEditable="true" id="text">Write your text...</h1>
+                <button id="download-btn" onClick={downloadImage}>Download</button>
+            </div>
+
+            <div id='child2'>
+                <div id='text-color'>
+                  <h2 id="sub-heading">Choose your text Color</h2>
+                  <div id="text-color-names">
+                    <button id='red' onClick={()=>{textColor('red')}}></button>
+                    <button id='green' onClick={()=>{textColor('green')}}></button>
+                    <button id='blue' onClick={()=>{textColor('blue')}}></button>
+                    <button id='yellow' onClick={()=>{textColor('yellow')}}></button>
+                    <button id='purple' onClick={()=>{textColor('purple')}}></button>
+                    
+                    <input 
+                    type='color'
+                    value={tcolor}
+                    onChange={(event)=>{
+                      setTColor(event.target.value)
+                      textColor(event.target.value)
+                    }}
+                    />
+                  </div>
+
+                </div>
+
+                <div id='bg-color'>
+                  <h2 id='sub-heading'>Choose your Background Color</h2>
+                  <div id="text-color-names">
+                    <button id='red' onClick={()=>{bgColor('red')}}></button>
+                    <button id='green' onClick={()=>{bgColor('green')}}></button>
+                    <button id='blue' onClick={()=>{bgColor('blue')}}></button>
+                    <button id='yellow' onClick={()=>{bgColor('yellow')}}></button>
+                    <button id='purple' onClick={()=>{bgColor('purple')}}></button>
+                    <input 
+                    type='color'
+                    value={bgCol}
+                    onChange={(event)=>{
+                      setBgCol(event.target.value)
+                      bgColor(event.target.value)
+                    }}
+                    />
+                  </div>
+                </div>
+
+                <div id='fonts'>
+                      <h1 id='times-new' onClick={()=>{fontFamily('Times New Roman')}}>Hello</h1>
+                      <h1 id='cursive' onClick={()=>{fontFamily('cursive')}}>Hello</h1>
+                      <h1 id='verdana' onClick={()=>{fontFamily('verdana')}}>Hello</h1>
+                      <h1 id='arial'  onClick={()=>{fontFamily('arial')}}>Hello</h1>
+                      <h1 id='impact' onClick={()=>{fontFamily('Impact')}}>Hello</h1>
+                </div>
+            </div>
+        </div>
+
+
+       
+    </>
+  );
 }
